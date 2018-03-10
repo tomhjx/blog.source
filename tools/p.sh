59b0a35fcc16cbe6ffc1cb188a2f9ec679d3eaa0
Tom <tom_hejiaxi@163.com>
2018-03-10 16:56:50 +0800

    更新配置

--- tools/p.sh
+++ tools/p.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 #发布到网站
-
-#手动发布到git
-#path=$(pwd)/public
-#cd ${path}
-#hexo g
-#----git
-#git add -A
-#git commit -m "Site updated."
-#git pull git@github.com:tomhjx/tomhjx.github.io.git master
-#git push git@github.com:tomhjx/tomhjx.github.io.git master
-
-#hexo工具发布到git
-hexo deploy
-
+path=$(pwd)/public
+mkdir -p ${path}
+cd ${path}
+hexo g -d
 
