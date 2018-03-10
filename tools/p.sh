d8bacd2742834345c5c4d9f66d32fd4ae7cc70f7
Tom <tom_hejiaxi@163.com>
2018-03-10 16:06:40 +0800

    修改发布方式

--- tools/p.sh
+++ tools/p.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 #发布到网站
-path=$(pwd)/public
-cd ${path}
-hexo g
+
+#手动发布到git
+#path=$(pwd)/public
+#cd ${path}
+#hexo g
 #----git
-git add -A
-git commit -m "Site updated."
-git pull git@github.com:tomhjx/tomhjx.github.io.git master
-git push git@github.com:tomhjx/tomhjx.github.io.git master
+#git add -A
+#git commit -m "Site updated."
+#git pull git@github.com:tomhjx/tomhjx.github.io.git master
+#git push git@github.com:tomhjx/tomhjx.github.io.git master
+
+#hexo工具发布到git
+hexo deploy
+
+
